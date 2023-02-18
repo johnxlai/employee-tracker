e1bdccd6ecf434eb4623f906829d44990b4571a2
John Lai <lai.john.py@gmail.com>
2023-02-18 18:42:17 -0500

    Fix schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,19 +11,17 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
-)
+);
 
 CREATE TABLE employee (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR(30) NOT NULL,
    last_name VARCHAR(30) NOT NULL,
-   role_id INT NOT NULL,
+   role_id INT,
    manager_id INT NOT NULL,
    FOREIGN KEY (role_id)
    REFERENCES role(id)
-
-)
+);
