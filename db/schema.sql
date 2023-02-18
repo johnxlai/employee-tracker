75216c4e621945f11535d7a7fed9fd5c70bba74a
John Lai <lai.john.py@gmail.com>
2023-02-18 16:19:08 -0500

    Updat schema sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employee_tracker_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
@@ -13,4 +13,14 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+)
+
+CREATE TABLE employee (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INT NOT NULL,
+   manager_id INT NOT NULL
 )
