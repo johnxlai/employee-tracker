4b1850c05be69bb06f388b6864d2a5c66a0da79a
John Lai <lai.john.py@gmail.com>
2023-02-18 01:23:42 -0500

    Update index js

---
+++ index.js
@@ -0,0 +1,36 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'logoName',
+      message: 'Please enter text, must not be more than 3 letters',
+    },
+    {
+      type: 'input',
+      name: 'textColour',
+      message: `Please enter text colour keyword or a hexadecimal number for the logo's test color`,
+    },
+    {
+      type: 'input',
+      name: 'logoColour',
+      message: `Please enter a colour keyword or a hexadecimal number for the logo's background colour`,
+    },
+    {
+      type: 'list',
+      name: 'logoShape',
+      message: `Please choose logo shape`,
+      choices: ['triangle', 'circle', 'square'],
+    },
+  ])
+  .then((data) => {
+    // const svgPath = './dist/logo.svg';
+    // const finalLogo = makeShape(data);
+    //Generate the svg logo here.
+    // fs.writeFile(svgPath, generateSvg(finalLogo), (err) =>
+    //   err ? console.error(err) : console.log('Generated logo.svg')
+    // );
+  })
+  .catch((err) => console.error(err));
