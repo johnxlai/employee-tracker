c3e8f40861a2af872752ea5b7662400ab86399e0
John Lai <lai.john.py@gmail.com>
2023-02-18 21:12:21 -0500

    Update seeds with departments and roles

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (id, department_name)
+VALUES  ("Sales"),
+        ("Advertising"),
+        ("Marketing"),
+        ("Development"),
+        ("Customer Service");
+
+INSERT INTO role ( title, department_id )
+VALUES  ("Software Dev" , 4),
+        ("Marketing Manager" , 3),
+        ("Project Manager" , 3),
+        ("Art Director" , 2),
+        ("Copy Writer" , 2),
+        ("Sales Manager" , 1),
+        ("Sales Rep" , 1),
+        ("Front End Developers" , 4),
+        ("Software Developer" , 4);
+        ("Backend Developer" , 4);
+
+
