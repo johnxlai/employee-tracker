d4bd732dbe132b34bac906ebc09e723245c70d0e
John Lai <lai.john.py@gmail.com>
2023-02-21 16:20:56 -0500

    Fix data query sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,35 +1,35 @@
 INSERT INTO department (name)
-VALUES  ("Sales"),
-        ("Advertising"),
+VALUES  ("Development"),
         ("Marketing"),
-        ("Development"),
-        ("Customer Service");
+        ("Sales"),
+        ("Advertising");
 
 INSERT INTO role ( title, salary, department_id )
-VALUES  ("Tech Lead" , 900000.00, 4),
-        ("Marketing Manager" , 100000.00, 3),
-        ("Project Manager" , 50000.00, 3),
-        ("Art Director" ,134000.00, 2),
-        ("Copy Writer" ,156000.00, 2),
-        ("Sales Manager" ,121000.00, 1),
-        ("Sales Rep" ,129000.00, 1),
-        ("Front End Developers" ,310000.00, 4),
-        ("Software Developer" ,510000.00, 4),
-        ("Backend Developer" ,310000.00, 4);
+VALUES  ("Tech Lead" , 900000.00, 1),
+        ("Front End Developers" ,310000.00, 1),
+        ("Software Developer" ,510000.00, 1),
+        ("Backend Developer" ,310000.00, 1),
+        ("Marketing Manager", 100000.00, 2),
+        ("Marketing Specialist" , 60000.00, 2),
+        ("Sales Manager" , 129000.00, 3),
+        ("Sales Rep" ,70000.00, 3),
+        ("Advertising Manager", 200000.00, 4),
+        ("Art Director" ,134000.00, 4),
+        ("Copy Writer" ,156000.00, 4);
 
 INSERT INTO employee ( first_name, last_name, role_id, manager_id)
-VALUES  ("John", "Lai", 9, null),
-        ("Dan", "Taylor", 1, 3),
-        ("Taylor", "Smith", 3, null),
-        ("Jack", "Ma", 2, 5),
+VALUES  ("Tom", "Hanks", 9, null),
+        ("Taylor", "Swift", 1, 1),
+        ("Thor", "Smith", 3, null),
+        ("Steve", "Rogers", 2, 2),
         ("Elon", "Musk", 8, null),
-        ("Tim", "Cook", 7, 8),
-        ("Skylar", "Water", 5, null ),
-        ("Paige", "Gerrad", 4, 1),
-        ("Sam", "Smith", 6, null),
+        ("Tim", "Cook", 7, 3),
+        ("Skylar", "Grey", 5, null ),
+        ("Paige", "Gerrad", 4, 4),
+        ("Sam", "Smith", 6, 2),
         ("Adele", "Notsure", 2, 4),
-        ("Sam", "Drinkwater", 4, 5),
-        ("Mark", "Messi", 5, 6),
-        ("Sarah", "Ronaldo", 6, 7),
-        ("Zara", "Neymar", 7, 1),
-        ("Mary", "Me", 10, 7);
+        ("Sam", "Drinkwater", 4, 3),
+        ("Mark", "Messi", 5, 1),
+        ("Sarah", "Ronaldo", 6, 2),
+        ("Zara", "Neymar", 11, 1),
+        ("Mary", "Me", 10, 4);
