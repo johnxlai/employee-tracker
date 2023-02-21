78b97cfb2337eed379165572273b0b8bd620ef9b
John Lai <lai.john.py@gmail.com>
2023-02-21 15:59:56 -0500

    Fix seeds and manager statement

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,25 +6,25 @@ VALUES  ("Sales"),
         ("Customer Service");
 
 INSERT INTO role ( title, salary, department_id )
-VALUES  ("Software Dev" , 123.12, 4),
-        ("Marketing Manager" ,123.12, 3),
-        ("Project Manager" ,123.54, 3),
-        ("Art Director" ,123.12, 2),
-        ("Copy Writer" ,123.12, 2),
-        ("Sales Manager" ,123.12, 1),
-        ("Sales Rep" ,123.12, 1),
-        ("Front End Developers" ,123.13, 4),
-        ("Software Developer" ,123.12, 4),
-        ("Backend Developer" ,123.12, 4);
+VALUES  ("Tech Lead" , 900000.00, 4),
+        ("Marketing Manager" , 100000.00, 3),
+        ("Project Manager" , 50000.00, 3),
+        ("Art Director" ,134000.00, 2),
+        ("Copy Writer" ,156000.00, 2),
+        ("Sales Manager" ,121000.00, 1),
+        ("Sales Rep" ,129000.00, 1),
+        ("Front End Developers" ,310000.00, 4),
+        ("Software Developer" ,510000.00, 4),
+        ("Backend Developer" ,310000.00, 4);
 
 INSERT INTO employee ( first_name, last_name, role_id, manager_id)
 VALUES  ("John", "Lai", 9, null),
         ("Dan", "Taylor", 1, 3),
-        ("Taylor", "Smith", 3, 2),
+        ("Taylor", "Smith", 3, null),
         ("Jack", "Ma", 2, 5),
         ("Elon", "Musk", 8, null),
         ("Tim", "Cook", 7, 8),
-        ("Skylar", "Water", 5, 7 ),
+        ("Skylar", "Water", 5, null ),
         ("Paige", "Gerrad", 4, 1),
         ("Sam", "Smith", 6, null),
         ("Adele", "Notsure", 2, 4),
